acfd929756a91815483c37590e24d99abe313852
anjal <anjal@LAPTOP-RV92P6RL>
2018-10-17 23:09:27 +0530

    second commit

--- MyProject/src/newproject/newone.java
+++ MyProject/src/newproject/newone.java
@@ -32,7 +32,7 @@ public class newone {
 		str2 = str1.substring(0, str1.length()-str2.length());
 		str1 = str1.substring(str2.length());
 		
-		System.out.println("After "+ str1 +""+str2);
+		System.out.println("After "+ str1 +""+str2); 
 	}
 	
 	
